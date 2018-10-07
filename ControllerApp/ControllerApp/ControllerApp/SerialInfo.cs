e9d2ec2156e0347af05a35cabf3fbad9d3327c33
Deshan Alahakoon <deshan@calcey.com>
2018-10-07 14:22:20 +0530

    steering and throtle micro controller program added

---
+++ ControllerApp/ControllerApp/ControllerApp/SerialInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Ports;
+namespace ControllerApp
+{
+    public class SerialInfo
+    {
+
+        public static void GetPorts()
+        {
+            Console.WriteLine("Serial ports available:");
+            Console.WriteLine("-----------------------");
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(portName);
+            }
+        }
+
+    }
+}
--- main/main.ino
+++ main/main.ino
@@ -1,18 +1,49 @@
-int led_pin = 13;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(led_pin, OUTPUT);
+const int STEERING_DOUT = 9;
+const int THROTTLE_DOUT = 10;
 
-  digitalWrite(led_pin, HIGH);
+String incomingString;
+String tempValue;
+int steeringValue = 0;
+int throttleValue = 0;
 
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(led_pin, HIGH);
-  delay(500);
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    // read the incoming string:
+    incomingString = Serial.readString();
 
-  digitalWrite(led_pin, LOW);
+    tempValue = getValue(incomingString, ';', 0);
+    steeringValue = tempValue.toInt();
 
-  delay(500);
+    tempValue = getValue(incomingString, ';', 1);
+    throttleValue = tempValue.toInt();
+
+    analogWrite(STEERING_DOUT, steeringValue);
+    analogWrite(THROTTLE_DOUT, throttleValue);
+  }
+}
+
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++)
+  {
+    if (data.charAt(i) == separator || i == maxIndex)
+    {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
